d3b564f5020f406e1ff76e2697458d28618d8295
Brendan P. McCoy <bpmccoy@admnet.oakland.edu>
2016-05-11 16:24:00 -0400

    Signed-off-by: Brendan P. McCoy <bpmccoy@admnet.oakland.edu>

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,37 @@
+﻿$packageName= 'cockatrice'
+$url        = 'https://github.com/Cockatrice/Cockatrice/releases/download/2016-05-06-Release/Cockatrice_The_Shadows_win32.exe'
+$url64      = 'https://github.com/Cockatrice/Cockatrice/releases/download/2016-05-06-Release/Cockatrice_The_Shadows_win64.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs   = '/S'
+
+  softwareName  = 'cockatrice*'
+  checksum      = '0430270ACA355BACC99CA288A9067F1D45417AE4'
+  checksumType  = 'sha1'
+  checksum64    = '66061451D8526C4152EC04CDDBA807AC45C4826D'
+  checksumType64= 'sha1'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
